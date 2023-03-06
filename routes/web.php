11874e82eba5f669d5520a3c547e96101f1d34f5
ivbruno <ivanbruno222@gmail.com>
2023-03-06 15:10:16 +0200

    Starting adventure with Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,7 @@
-<?php
+<!doctype html>
 
-use Illuminate\Support\Facades\Route;
+<title>My Blog</title>
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+<body>
+    <h1>Hello World!</h1>
+</body>
